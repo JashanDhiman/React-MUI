7b84a393092dd2da579af4a3c1e1b98f4756787a
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-15 22:48:52 +0530

    practiced text fields

--- src/components/MuiTextField.tsx
+++ src/components/MuiTextField.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
-
+import { InputAdornment, Stack, TextField } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useState } from "react";
 const MuiTextField = () => {
+  const [value, setValue] = useState("");
   return (
-    <div>MuiTextField</div>
-  )
-}
+    <Stack spacing={4}>
+      <Stack direction="row" spacing={2}>
+        <TextField label="Name" variant="outlined" />
+        <TextField label="Name" variant="filled" />
+        <TextField label="Name" variant="standard" />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField label="Small secondory" size="small" color="secondary" />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="Form Input"
+          required
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          error={!value}
+          helperText={!value ? "Required" : "do not share password with anyone"}
+        />
+        <TextField
+          label="Form Input"
+          type="password"
+          helperText="do not share password with anyone"
+        />
+      </Stack>
+      <Stack direction="row" spacing={2}>
+        <TextField
+          label="amount"
+          InputProps={{
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="weight"
+          InputProps={{
+            endAdornment: <InputAdornment position="end">kg</InputAdornment>,
+          }}
+        />
+        <TextField
+          label="password"
+          type="password"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <VisibilityIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+    </Stack>
+  );
+};
 
-export default MuiTextField
+export default MuiTextField;
