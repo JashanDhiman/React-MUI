e4bc129e664eddc5113795cda13910f7996b93d7
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-17 22:25:45 +0530

    practiced radio buttons

--- src/components/MuiRadioButton.tsx
+++ src/components/MuiRadioButton.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { useState } from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+} from "@mui/material";
 
 const MuiRadioButton = () => {
+  const [value, setValue] = useState("");
+  console.log({ value });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(React.ChangeEvent<HTMLInputElement>);
+    setValue(event.target.value);
+  };
   return (
-    <div>MuiRadioButton</div>
-  )
-}
+    <Box>
+      <FormControl>
+        <FormLabel id="job-experience-group-label">
+          Years of experience
+        </FormLabel>
+        <RadioGroup
+          name="job-experience-group"
+          aria-label="job-experience-group-label"
+          value={value}
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel
+            control={<Radio size="small" color="secondary" />}
+            label="0-2"
+            value="0-2"
+          />
+          <FormControlLabel control={<Radio />} label="3-5" value="3-5" />
+          <FormControlLabel control={<Radio />} label="6-9" value="6-9" />
+        </RadioGroup>
+      </FormControl>
+    </Box>
+  );
+};
 
-export default MuiRadioButton
+export default MuiRadioButton;
