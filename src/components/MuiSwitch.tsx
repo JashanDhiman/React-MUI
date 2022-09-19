bcdd6c892a77d198777642b79a143895e362b735
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-19 23:31:36 +0530

    practiced Switch buttons

--- src/components/MuiSwitch.tsx
+++ src/components/MuiSwitch.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Box,
+  FormControlLabel,
+  Switch,
+  FormControl,
+  FormLabel,
+  FormGroup,
+} from "@mui/material";
 
 const MuiSwitch = () => {
+  const [checked, setChecked] = useState(false);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+  console.log({ checked });
   return (
-    <div>MuiSwitch</div>
-  )
-}
+    <>
+      <Box>
+        <FormControlLabel
+          label="Dark mode"
+          control={
+            <Switch checked={checked} onChange={handleChange} color="success" />
+          }
+        />
+      </Box>
+      <Box>
+        <FormControl>
+          <FormLabel>Skills</FormLabel>
+          <FormGroup row>
+            <FormControlLabel
+              label="html"
+              value="html"
+              control={<Switch checked={checked} onChange={handleChange} />}
+            />
+            <FormControlLabel
+              label="css"
+              value="css"
+              control={<Switch checked={checked} onChange={handleChange} />}
+            />
+            <FormControlLabel
+              label="js"
+              value="js"
+              control={<Switch checked={checked} onChange={handleChange} />}
+            />
+          </FormGroup>
+        </FormControl>
+      </Box>
+    </>
+  );
+};
 
-export default MuiSwitch
+export default MuiSwitch;
