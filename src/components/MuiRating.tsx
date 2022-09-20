08b96fc21d143b228d352a64a71950461eba60ec
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-20 23:36:46 +0530

    practiced Rating buttons

--- src/components/MuiRating.tsx
+++ src/components/MuiRating.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
+import { Rating, Stack } from "@mui/material";
+import React, { useState } from "react";
 
 const MuiRating = () => {
+  const [rating, setRating] = useState<number | null>(null);
+  const handleRating = (
+    _event: React.ChangeEvent<{}>,
+    newValue: number | null
+  ) => {
+    setRating(newValue);
+  };
+  console.log(rating);
   return (
-    <div>MuiRating</div>
-  )
-}
+    <Stack spacing={2}>
+      <Rating
+        value={rating}
+        onChange={handleRating}
+        precision={0.5}
+        size="large"
+        icon={<Favorite fontSize="inherit" />}
+        emptyIcon={<FavoriteBorder fontSize="inherit" />}
+      />
+    </Stack>
+  );
+};
 
-export default MuiRating
+export default MuiRating;
