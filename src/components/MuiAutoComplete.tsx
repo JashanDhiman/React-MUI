211ed8a90cbb938a3b28a16939bd42d6f3900eb9
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-21 23:59:46 +0530

    practiced auto complete drop down menu

--- src/components/MuiAutoComplete.tsx
+++ src/components/MuiAutoComplete.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { Autocomplete, Stack, TextField } from "@mui/material";
+import React, { useState } from "react";
 
-const MuiAutoComplete = () => {
+type Skill = {
+  id: number;
+  label: string;
+};
+const skills = ["html", "javascript", "typescript", "python", "css"];
+const skillsOptions = skills.map((skill, index) => ({
+  id: index + 1,
+  label: skill,
+}));
+function MuiAutoComplete() {
+  const [value, setValue] = useState<string | null>(null);
+  const [skill, setSkill] = useState<Skill | null>(null);
+  console.log(skill);
   return (
-    <div>MuiAutoComplete</div>
-  )
+    <Stack spacing={2} width="250px">
+      <Autocomplete
+        options={skills}
+        renderInput={(params) => <TextField {...params} label="skills" />}
+        value={value}
+        onChange={(event: any, newValue: string | null) => setValue(newValue)}
+        freeSolo
+      />
+      <Autocomplete
+        options={skillsOptions}
+        renderInput={(params) => <TextField {...params} label="skills" />}
+        value={skill}
+        onChange={(event: any, newValue: Skill | null) => setSkill(newValue)}
+      />
+    </Stack>
+  );
 }
 
-export default MuiAutoComplete
+export default MuiAutoComplete;
