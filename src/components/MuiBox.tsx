01f6d1bdb00a54a111d2cf838b05f05d553dba44
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-22 23:19:46 +0530

    practiced box component

--- src/components/MuiBox.tsx
+++ src/components/MuiBox.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { Box, Divider, Stack } from "@mui/material";
 
 const MuiBox = () => {
   return (
-    <div>MuiBox</div>
-  )
-}
+    <Stack
+      sx={{ border: "1px solid" }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
+    >
+      <Box
+        sx={{
+          backgroundColor: "primary.main",
+          color: "white",
+          height: "2rem",
+          width: "2rem",
+          padding: "1rem",
+          "&:hover": {
+            backgroundColor: "primary.light",
+          },
+        }}
+      >
+        hello jashan
+      </Box>
+      <Box
+        display={"flex"}
+        height="100px"
+        width="fit-content"
+        bgcolor="success.light"
+        p={3}
+      >
+        jashan
+      </Box>
+    </Stack>
+  );
+};
 
-export default MuiBox
+export default MuiBox;
