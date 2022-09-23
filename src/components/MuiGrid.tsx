e0f7cbcca49c05328774c1ff23f00ced91d609d7
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-23 23:12:00 +0530

    practiced grid component

--- src/components/MuiGrid.tsx
+++ src/components/MuiGrid.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Box, Grid } from "@mui/material";
 
 const MuiGrid = () => {
   return (
-    <div>MuiGrid</div>
-  )
-}
+    <Grid container my={4} rowSpacing={1} columnSpacing={6}>
+      <Grid item xs={12} sm={6}>
+        <Box bgcolor="primary.light" p={2}>
+          item 1
+        </Box>
+      </Grid>
+      <Grid item xs={"auto"} sm={6}>
+        <Box bgcolor="primary.light" p={2}>
+          item 2
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Box bgcolor="primary.light" p={2}>
+          item 3
+        </Box>
+      </Grid>
+      <Grid item xs={12} sm={"auto"}>
+        <Box bgcolor="primary.light" p={2}>
+          item 4
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
 
-export default MuiGrid
+export default MuiGrid;
