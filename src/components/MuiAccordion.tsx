132a5d8fd6983b97119e99f71b7c7d4b977846fc
JashanDhiman <jashandhiman.hulk@gmail.com>
2022-09-25 23:12:45 +0530

    practiced accordion component

--- src/components/MuiAccordion.tsx
+++ src/components/MuiAccordion.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import { ExpandMore } from "@mui/icons-material";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 const MuiAccordion = () => {
+  const [expanded, setExpanded] = useState<string | false>(false);
+  const handleChange = (isExpanded: boolean, panel: string) => {
+    setExpanded(isExpanded ? panel : false);
+  };
   return (
-    <div>MuiAccordion</div>
-  )
-}
+    <div>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel1")}
+      >
+        <AccordionSummary
+          id="panel1-header"
+          aria-controls="panel1-content"
+          expandIcon={<ExpandMore />}
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi a
+            rerum soluta sapiente ullam hic totam nisi nemo ex assumenda
+            provident aperiam harum, sint doloremque explicabo tempora fugiat
+            dolorem?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel2")}
+      >
+        <AccordionSummary
+          id="panel2-header"
+          aria-controls="panel2-content"
+          expandIcon={<ExpandMore />}
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi a
+            rerum soluta sapiente ullam hic totam nisi nemo ex assumenda
+            provident aperiam harum, sint doloremque explicabo tempora fugiat
+            dolorem?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel3")}
+      >
+        <AccordionSummary
+          id="panel3-header"
+          aria-controls="panel3-content"
+          expandIcon={<ExpandMore />}
+        >
+          <Typography>Accordion 3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit modi a
+            rerum soluta sapiente ullam hic totam nisi nemo ex assumenda
+            provident aperiam harum, sint doloremque explicabo tempora fugiat
+            dolorem?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
 
-export default MuiAccordion
+export default MuiAccordion;
